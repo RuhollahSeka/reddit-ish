7679e8c7dc52997e929d3e03138fdbdd5a83084e
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-31 21:48:11 +0330

    Add user profile model

---
+++ users/models/user_profile.py
@@ -0,0 +1,31 @@
+from decouple import config
+from django.contrib.auth.models import User
+from django.db import models
+
+from _helpers.db.models import TimeModel
+from _helpers.db.utils import RenameOnUpload
+
+
+class UserProfile(TimeModel):
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        verbose_name='کاربر',
+    )
+
+    avatar = models.ImageField(
+        upload_to=RenameOnUpload(config('PROFILE_IMAGES_DIR')),
+        null=True,
+        blank=True,
+        verbose_name='عکس پروفایل'
+    )
+
+    location = models.CharField(
+        max_length=128,
+        blank=True,
+        verbose_name='مکان'
+    )
+
+    reset_password_uuid = models.UUIDField(
+        verbose_name='کد مربوط به لینک فراموشی رمز'
+    )
