217ed37f70b85564db053a0384195b5f1c66b367
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-31 17:48:40 +0330

    Add Content abstract model

---
+++ posts/models/content.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from _helpers.db.models import TimeModel
+
+
+class Content(TimeModel):
+    text = models.TextField(
+        verbose_name='متن'
+    )
+
+    up_votes = models.PositiveIntegerField(
+        default=0,
+        verbose_name='تعداد بازخوردهای مثبت'
+    )
+
+    down_votes = models.PositiveIntegerField(
+        default=0,
+        verbose_name='تعداد بازخوردهای منفی'
+    )
+
+    @property
+    def score(self):
+        return self.up_votes - self.down_votes
+
+    class Meta:
+        abstract = True
