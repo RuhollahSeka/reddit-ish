87193e33ba02c12f699cdbef3f55cc807b595e38
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-01 04:00:33 +0330

    Create user profile on user creation

--- users/serializers/user.py
+++ users/serializers/user.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from users.models import UserProfile
+
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -9,3 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'email', 'password')
         read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        new_user = super().create(validated_data)
+        UserProfile.objects.create(user=new_user)
+        return new_user
