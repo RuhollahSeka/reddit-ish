dcd92095806e302223d5ff23665fd15f352b9ee5
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-03 21:54:03 +0330

    Add post urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from posts.views import PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, 'posts')
+
 
 urlpatterns = [
 
 ]
+
+urlpatterns += router.urls
