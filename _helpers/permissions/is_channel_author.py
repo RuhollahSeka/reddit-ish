2f708196b6a880cd1f6a42928f8ac9d747683e46
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-03 23:30:47 +0330

    Add IsChannelAuthor permission

---
+++ _helpers/permissions/is_channel_author.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+from posts.models import Channel
+
+
+class IsChannelAuthor(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        channel_id = request.data.get('channel', 0)
+        if not channel_id or not isinstance(channel_id, int):
+            return False
+        channel = Channel.objects.filter(id=channel_id).first()
+        return channel and channel.authors.filter(id=user).exists()
