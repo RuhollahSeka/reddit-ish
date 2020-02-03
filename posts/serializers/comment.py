1700f7dafd53aa2881745660a31453895ab195c4
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-03 06:23:41 +0330

    Add parent post and parent comment to comment serializer

--- posts/serializers/comment.py
+++ posts/serializers/comment.py
@@ -9,10 +9,14 @@ class CommentSerializer(serializers.ModelSerializer):
     comments = RecursiveSerializer(many=True)
     up_voted = serializers.SerializerMethodField()
     down_voted = serializers.SerializerMethodField()
+    parent_post = serializers.IntegerField(write_only=True, required=False)
+    parent_comment = serializers.IntegerField(write_only=True, required=False)
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'text', 'score', 'comments', 'up_voted', 'down_voted')
+        fields = (
+            'id', 'author', 'text', 'score', 'comments', 'up_voted', 'down_voted', 'parent_post', 'parent_comment'
+        )
         read_only_fields = ('id', 'score', 'comments', 'up_voted', 'down_voted')
 
     def get_up_voted(self, instance: Comment):
