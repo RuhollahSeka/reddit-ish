27ef9a6eeb1df973042cf654f0d97007baed845a
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-04 00:43:04 +0330

    Refactor main list and retrieve post api

--- posts/views/post_views/post.py
+++ posts/views/post_views/post.py
@@ -1,13 +1,13 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from posts.models import Post
 from posts.serializers import PostRetrieveSerializer, PostListCreateSerializer
 
 
-class PostViewSet(ModelViewSet):
+class PostViewSet(ReadOnlyModelViewSet):
     authentication_classes = (JWTAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
 
@@ -18,7 +18,7 @@ class PostViewSet(ModelViewSet):
 
     def get_queryset(self):
         channel_id = self.kwargs.get('channel_id')
+        channel_filter = {'channel_id': channel_id} if channel_id else {}
         ordering_type = self.request.query_params.get('ordering')
         ordering_type = ['-' + ordering_type] if ordering_type else []
-        channel_filter = {'channel_id': channel_id} if channel_id else {}
         return Post.objects.filter(**channel_filter).order_by(*ordering_type)
