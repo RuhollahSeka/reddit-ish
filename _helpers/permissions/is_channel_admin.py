037f4f0609bec4279686fc403510d5ccef978194
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-03 23:30:32 +0330

    Add IsChannelAdmin permission

---
+++ _helpers/permissions/is_channel_admin.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission
+
+from posts.models import Channel
+
+
+class IsChannelAdmin(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        channel_id = request.kwargs.get('pk', 0)
+        if not channel_id or not isinstance(channel_id, int):
+            return False
+        channel = Channel.objects.filter(id=channel_id).first()
+        return channel and channel.admin == user
