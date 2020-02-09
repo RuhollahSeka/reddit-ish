c9df9448e301cbc48dad1c7e42ba78716de2b04a
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-10 02:12:36 +0330

    Add throttles to user create API

--- users/views/user_create.py
+++ users/views/user_create.py
@@ -2,10 +2,12 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
 from users.serializers import UserSerializer
+from users.throttles import BurstAnonRateThrottle, SustainedAnonRateThrottle
 
 
 class UserCreateAPIView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = UserSerializer
+    throttle_classes = (BurstAnonRateThrottle, SustainedAnonRateThrottle)
 
 
