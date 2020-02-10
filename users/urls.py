fc2d9a6ee8b16499c72d3dd8bfcb86965415c1fb
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-10 09:11:56 +0330

    Complete user urls

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views import UserCreateAPIView
+from users.views.user_profile import UserProfileRetrieveUpdateAPIView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', UserCreateAPIView.as_view())
+    path('', UserCreateAPIView.as_view()),
+    path('profiles/', UserProfileRetrieveUpdateAPIView.as_view()),
 ]
