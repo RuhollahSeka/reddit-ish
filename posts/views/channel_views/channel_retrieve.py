6cc00d471bb3845da76f5e3f0e1a780ff9e8f444
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-10 10:40:14 +0330

    Fix channel retrieve queryset

--- posts/views/channel_views/channel_retrieve.py
+++ posts/views/channel_views/channel_retrieve.py
@@ -2,6 +2,7 @@ from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from posts.models import Channel
 from posts.serializers import ChannelListRetrieveSerializer
 
 
@@ -9,3 +10,4 @@ class ChannelRetrieveAPIView(RetrieveAPIView):
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = ChannelListRetrieveSerializer
+    queryset = Channel.objects.all()
